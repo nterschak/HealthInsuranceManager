e3bb2d40c704040c7ed2181fc949cff321378ed9
Nathan Terschak <nathanterschak@gmail.com>
2024-07-08 14:26:04 -0500

    Added navigation header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemberListComponent
+    MemberListComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
